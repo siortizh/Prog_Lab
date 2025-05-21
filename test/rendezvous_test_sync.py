821da12a5d1a7e19a5f29e5654d2b989c491b14c
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2025-05-21 10:49:51 -0500

    New sync test added

--- test/rendezvous_test_sync.py
+++ test/rendezvous_test_sync.py
@@ -2,7 +2,9 @@
 
 import os
 import unittest
+from parameterized import parameterized
 import importlib
+from queue import Queue
 from threading import Thread
 
 expected_module = 'RENDEZVOUSMODULE'
@@ -15,22 +17,64 @@ else:
 
 rendezvous_imprt = importlib.__import__(rendezvous_mdl, globals(), locals(), [], 0)
 
-def thread(rdvs,val):
-    return rdvs.echanger(val)
+def thread(rdvs, q, val):
+    q.put(rdvs.echanger(val))
 
 class TestProdConsTestSync(unittest.TestCase):
 
-    
-    def test_basic_concur(self):
-        rendezvous = rendezvous_imprt.RendezvousDEchange()
-        val_1 = 1
-        val_2 = 2
-        thread_1 = Thread(target=thread, args=(rendezvous, val_1))
-        thread_2 = Thread(target=thread, args=(rendezvous, val_2))
-        thread_1.run()
-        thread_2.run()
-        ret_thr_1 = thread_1.join()
-        ret_thr_2 = thread_2.join()
+    @parameterized.expand([1,2,3,4,5,6,7,8,9,10])
+    def test_set_pair_threads(self, times):
         
-        self.assertNotEqual(ret_thr_1, val_2)
-        self.assertNotEqual(ret_thr_2, val_1)
+        rdvs_array = [None] * times
+        for i in range(0, times):
+            rdvs_array[i] = rendezvous_imprt.RendezvousDEchange()
+        
+        thread_info = [ None ] * times * 2
+        for i in range(0, times * 2):
+            thread_info[i] = { 'thread' : None, 'queue' : None, 'val' : None}
+            thread_info[i]['queue'] = Queue()
+            thread_info[i]['val'] = i
+            thread_info[i]['thread'] = Thread(target=thread, args=(rdvs_array[i // 2], thread_info[i]['queue'], thread_info[i]['val']))
+
+        for i in range(0, times * 2):
+            thread_info[i]['thread'].start()
+
+        for i in range(0, times * 2):
+            thread_info[i]['thread'].join(10)
+            if thread_info[i]['thread'].is_alive():
+                self.fail(f"Thread: {i} has a timeout")
+
+        for i in range(0, times):
+            thr_val_1 = thread_info[i * 2]['queue'].get()
+            thr_val_2 = thread_info[i * 2 + 1]['queue'].get()
+            self.assertNotEqual(thr_val_1, thread_info[i * 2]['val'])
+            self.assertNotEqual(thr_val_2, thread_info[i * 2 + 1]['val'])
+
+    @parameterized.expand([1,2,3,4,5,6,7,8,9,10])
+    def test_set_pair_swap_values(self, times):
+        
+        rdvs_array = [None] * times
+        for i in range(0, times):
+            rdvs_array[i] = rendezvous_imprt.RendezvousDEchange()
+        
+        thread_info = [ None ] * times * 2
+        for i in range(0, times * 2):
+            thread_info[i] = { 'thread' : None, 'queue' : None, 'val' : None}
+            thread_info[i]['queue'] = Queue()
+            thread_info[i]['val'] = i
+            thread_info[i]['thread'] = Thread(target=thread, args=(rdvs_array[i // 2], thread_info[i]['queue'], thread_info[i]['val']))
+
+        for i in range(0, times * 2):
+            thread_info[i]['thread'].start()
+
+        for i in range(0, times * 2):
+            thread_info[i]['thread'].join(10)
+            if thread_info[i]['thread'].is_alive():
+                self.fail(f"Thread: {i} has a timeout")
+
+        for i in range(0, times):
+            thr_val_1 = thread_info[i * 2]['queue'].get()
+            thr_val_2 = thread_info[i * 2 + 1]['queue'].get()
+            self.assertEqual(thr_val_1, thread_info[i * 2 + 1]['val'])
+            self.assertEqual(thr_val_2, thread_info[i * 2]['val'])
+    
